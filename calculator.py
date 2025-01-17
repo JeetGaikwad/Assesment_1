2ff4153f93e026c717a8757135df78adfc51378e
Jeet Gaikwad <jeetgaikwad95@gmail.com>
2025-01-17 17:31:23 +0530

    Modulo Operation

--- calculator.py
+++ calculator.py
@@ -1,12 +1,12 @@
 class Calculator:
-    # Division of Two numbers
-    def div(self, a, b):
-        return a / b
+    # Modulo of Two numbers
+    def mod(self, a, b):
+        return a % b
     
     
 
 calculate = Calculator()
 inp1 = int(input("Enter first number: "))
 inp2 = int(input("Enter second number: "))
-print(calculate.div(inp1, inp2))
+print(calculate.mod(inp1, inp2))
     
