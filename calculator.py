764ae92718941a96e44db4e6ff7ac798891799ac
Jeet Gaikwad <jeetgaikwad95@gmail.com>
2025-01-17 17:47:44 +0530

    Removed file errors

--- calculator.py
+++ calculator.py
@@ -1,28 +1,19 @@
 class Calculator:
-<<<<<<< HEAD
-<<<<<<< HEAD
     # Subtraction of Two numbers
     def sub(self, a, b):
         return a - b
-=======
     # Multiplication of Two numbers
     def mul(self, a, b):
         return a * b
->>>>>>> calculate/mul
-=======
     # Division of Two numbers
     def div(self, a, b):
         return a / b
->>>>>>> calculate/div
     
     
 
 calculate = Calculator()
 inp1 = int(input("Enter first number: "))
 inp2 = int(input("Enter second number: "))
-<<<<<<< HEAD
 print(calculate.mul(inp1, inp2))
-=======
 print(calculate.div(inp1, inp2))
->>>>>>> calculate/div
     
