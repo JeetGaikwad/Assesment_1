2b19a10441122b53dba6aa58ac12c8d047357ba4
Jeet Gaikwad <jeetgaikwad95@gmail.com>
2025-01-17 17:27:07 +0530

    Division Operation

--- calculator.py
+++ calculator.py
@@ -1,12 +1,12 @@
 class Calculator:
-    # Subtraction of Two numbers
-    def sub(self, a, b):
-        return a - b
+    # Division of Two numbers
+    def div(self, a, b):
+        return a / b
     
     
 
 calculate = Calculator()
 inp1 = int(input("Enter first number: "))
 inp2 = int(input("Enter second number: "))
-print(calculate.sub(inp1, inp2))
+print(calculate.div(inp1, inp2))
     
