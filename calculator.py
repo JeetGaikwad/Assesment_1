5479ee52c596c47ba2fabe880bcd6faaecab8e1f
Jeet Gaikwad <jeetgaikwad95@gmail.com>
2025-01-17 17:24:57 +0530

    Multiplication Operation

--- calculator.py
+++ calculator.py
@@ -1,12 +1,12 @@
 class Calculator:
-    # Addition of Two numbers
-    def sub(self, a, b):
-        return a - b
+    # Multiplication of Two numbers
+    def mul(self, a, b):
+        return a * b
     
     
 
 calculate = Calculator()
 inp1 = int(input("Enter first number: "))
 inp2 = int(input("Enter second number: "))
-print(calculate.sub(inp1, inp2))
+print(calculate.mul(inp1, inp2))
     
