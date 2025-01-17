7891b1f376fd287787dbdc5788e07830f0fa873c
Jeet Gaikwad <jeetgaikwad95@gmail.com>
2025-01-17 17:21:00 +0530

    Addition operation

---
+++ calculator.py
@@ -0,0 +1,12 @@
+class Calculator:
+    # Addition of Two numbers
+    def add(self, a, b):
+        return a + b
+    
+    
+
+calculate = Calculator()
+inp1 = int(input("Enter first number: "))
+inp2 = int(input("Enter second number: "))
+print(calculate.add(inp1, inp2))
+    
