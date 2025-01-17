2a8858016c4c038dcfed52e5eb31b61b9c503ce6
Jeet Gaikwad <jeetgaikwad95@gmail.com>
2025-01-17 17:32:14 +0530

    Exponential Operation

--- calculator.py
+++ calculator.py
@@ -1,12 +1,12 @@
 class Calculator:
-    # Modulo of Two numbers
-    def mod(self, a, b):
-        return a % b
+    # Exponential of Two numbers
+    def exp(self, a, b):
+        return a ** b
     
     
 
 calculate = Calculator()
 inp1 = int(input("Enter first number: "))
 inp2 = int(input("Enter second number: "))
-print(calculate.mod(inp1, inp2))
+print(calculate.exp(inp1, inp2))
     
